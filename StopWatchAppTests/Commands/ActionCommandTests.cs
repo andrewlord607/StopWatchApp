6ae1815058bc92d327e8a924f7c2bceb452cd327
RayRelax <www.ordandrew1@gmail.com>
2021-07-06 17:50:13 +0300

    Add tests for ActionCommand

---
+++ StopWatchAppTests/Commands/ActionCommandTests.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace StopWatchApp.Tests
+{
+	[TestClass()]
+	public class ActionCommandTests
+	{
+
+		[TestMethod]
+		public void CanExecuteOverloadExecutesTruePredicate()
+		{
+			var command = new ActionCommand(p => { }, p => (int)p == 1);
+			Assert.IsTrue(command.CanExecute(1));
+		}
+
+		[TestMethod()]
+		public void CanExecuteOverloadExecutesFalsePredicate()
+		{
+			var command = new ActionCommand(p => { }, p => (int)p == 1);
+			Assert.IsFalse(command.CanExecute(0));
+		}
+
+		[TestMethod]
+		public void CanExecuteIsTrueByDefault()
+		{
+			var command = new ActionCommand(p => { });
+			Assert.IsTrue(command.CanExecute(null));
+		}
+
+		[TestMethod]
+		public void ExecuteOverloadInvokesActionWithParameter()
+		{
+			var invoked = false;
+			void action(object obj)
+			{
+				Assert.IsNotNull(obj);
+				invoked = true;
+			}
+
+			var command = new ActionCommand(action);
+			command.Execute(new object());
+
+			Assert.IsTrue(invoked);
+		}
+
+		[TestMethod]
+		public void ExecuteInvokesAction()
+		{
+			var invoked = false;
+
+			void action(object obj) => invoked = true;
+
+			var command = new ActionCommand(action);
+			command.Execute();
+
+			Assert.IsTrue(invoked);
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentNullException))]
+		public void ConstructorThrowsExceptionIfActionParameterIsNull()
+		{
+			_ = new ActionCommand(null);
+		}
+	}
+}
---
+++ StopWatchAppTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью указанного ниже 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// общие сведения об этой сборке.
+[assembly: AssemblyTitle("StopWatchAppTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StopWatchAppTests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка параметра ComVisible в значение false делает типы в этой сборке невидимыми 
+// для компонентов модели COM.  Если тип в этой сборке необходимо сделать доступным 
+// модели COM, задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("b4a54201-97c1-4243-8d63-d7b26a20a51a")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [сборка: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
